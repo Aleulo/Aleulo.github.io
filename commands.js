2479ca6226c1ec5f652bb183c27c6f748a419652
Aleulo <69992770+Aleulo@users.noreply.github.com>
2023-09-21 21:41:20 -0700

    Create commands.js
    file that has commands that would be printed

---
+++ commands.js
@@ -0,0 +1,78 @@
+
+const linkedin = "https://www.linkedin.com/comm/in/aleksandr-ivanov-a0841525b";
+const github = "https://github.com/Aleulo";
+export const email = 'mailto:alejandro.iff@gmail.com';
+
+export const whois = [
+  "<br>",
+  "Hi there, I'm Alex, and I'd like to introduce myself professionally.",
+"I'm experienced in both software development and cybersecurity, and I'm a recent college graduate.",
+"My background includes work in systems administration, cloud projects, and networking with Cisco equipment.",
+"I'm also skilled in using penetration testing tools for security assessments.",
+"I have a strong knack for troubleshooting and building reliable tech solutions, and I'm actively looking for new job opportunities.",
+  "<br>"
+];
+
+export const whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+export const social = [
+  "<br>",
+
+  'linkedin       <a href="' + linkedin + '" target="_blank">https://www.linkedin.com/comm/in/aleksandr-ivanov-a0841525b' + "</a>",
+  'github         <a href="' + github + '" target="_blank">https://github.com/Aleulo' + "</a>",
+  'Might add some other later',
+  "<br>"
+];
+
+
+
+export const projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+export const help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Aleulo?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  '<span class="command">sudo</span>           Unless you are an Admin!',
+  '<span class="command">email</span>          If you want to write me a letter...',
+  "<br>",
+];
+
+export const banner = [
+  '<span class="index">Aleulo (AI) Not A Corporation. Do WE have RIGHTS? </span>',
+'███████████████████████████ ██   ██  █████   ██████ ██   ██     ██ ██       ██████  ██    ██ ███████   ',
+'███████▀▀▀░░░░░░░▀▀▀███████ ██   ██ ██   ██ ██      ██  ██     ██  ██      ██    ██ ██    ██ ██          ',
+'████▀░░░░░░░░░░░░░░░░░▀████ ███████ ███████ ██      █████     ██   ██      ██    ██ ██    ██ █████      ',
+'███│░░░░░░░░░░░░░░░░░░░│███ ██   ██ ██   ██ ██      ██  ██   ██    ██      ██    ██  ██  ██  ██          ',
+'██▌│░░░░░░░░░░░░░░░░░░░│▐██ ██   ██ ██   ██  ██████ ██   ██ ██     ███████  ██████    ████   ███████    ',
+'██░└┐░░░░░░░░░░░░░░░░░┌┘░██',
+'██░░└┐░░░░░░░░░░░░░░░┌┘░░██',
+'██░░┌┘▄▄▄▄▄░░░░░▄▄▄▄▄└┐░░██ ██████  ██████  ███████  █████  ████████ ██   ██      █████  ██ ██████       ',
+'██▌░│██████▌░░░▐██████│░▐██ ██   ██ ██   ██ ██      ██   ██    ██    ██   ██     ██   ██ ██ ██   ██      ',
+'███░│▐███▀▀░░▄░░▀▀███▌│░███ ██████  ██████  █████   ███████    ██    ███████     ███████ ██ ██████       ',
+'██▀─┘░░░░░░░▐█▌░░░░░░░└─▀██ ██   ██ ██   ██ ██      ██   ██    ██    ██   ██     ██   ██ ██ ██   ██    ',
+'██▄░░░▄▄▄▓░░▀█▀░░▓▄▄▄░░░▄██ ██████  ██   ██ ███████ ██   ██    ██    ██   ██     ██   ██ ██ ██   ██      ',
+'████▄─┘██▌░░░░░░░▐██└─▄████',
+'█████░░▐█─┬┬┬┬┬┬┬─█▌░░█████',
+'████▌░░░▀┬┼┼┼┼┼┼┼┬▀░░░▐████  ██████  ███    ██     ██████   █████      ██   ██  ██████   ██████  ██████  ',
+'█████▄░░░└┴┴┴┴┴┴┴┘░░░▄█████ ██    ██ ████   ██     ██   ██ ██   ██     ██   ██ ██    ██ ██    ██ ██   ██ ',
+'███████▄░░░░░░░░░░░▄███████ ██    ██ ██ ██  ██     ██   ██ ███████     ███████ ██    ██ ██    ██ ██   ██ ',
+'██████████▄▄▄▄▄▄▄██████████ ██    ██ ██  ██ ██     ██   ██ ██   ██     ██   ██ ██    ██ ██    ██ ██   ██ ',
+'███████████████████████████  ██████  ██   ████     ██████  ██   ██     ██   ██  ██████   ██████  ██████  ',
+
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
+
